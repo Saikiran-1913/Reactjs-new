83dbb441ec84134da6bc07861986b93b723cb825
Saikiran-1913 <enumula1803@gmail.com>
2026-09-08 10:47:42 +0530

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ReactJS/',
+  base: '/Reactjs-new/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
